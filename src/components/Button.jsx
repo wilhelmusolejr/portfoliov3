bf0c7a841f6164af1148866c4a7ebe7f0eebf9ce
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-08-25 16:06:29 +0800

    added supporting props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-export default function Logo({ children, className = "" }) {
+export default function Logo({
+  children,
+  className = "",
+  href = "#",
+  target = "",
+}) {
   return (
     <>
-      <a className={`btn ${className}`} href="#">
+      <a className={`btn ${className}`} target={target} href={href}>
         {children}
       </a>
     </>
