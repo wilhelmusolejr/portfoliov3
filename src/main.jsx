69e041c3140c498564be9f83450433ecd46389d4
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-19 19:52:41 +0800

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
